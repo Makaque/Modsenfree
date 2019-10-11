ef1138ca3098e7acafb0a8d6f9eb49da814290c3
= <=>
2019-10-11 13:38:27 -0600

    Got table set up with checkboxes and resize policy

--- src/main/scala/ca/mktsk/modsenfree/Modsenfree.scala
+++ src/main/scala/ca/mktsk/modsenfree/Modsenfree.scala
@@ -3,13 +3,43 @@ package ca.mktsk.modsenfree
 
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
+import scalafx.beans.property.{BooleanProperty, ObjectProperty, StringProperty}
+import scalafx.beans.value.ObservableValue
+import scalafx.collections.ObservableBuffer
 import scalafx.geometry.Insets
 import scalafx.scene.Scene
+import scalafx.scene.control.cell.CheckBoxTableCell
+import scalafx.scene.control.{ScrollPane, TableColumn, TableView}
 import scalafx.scene.effect.DropShadow
-import scalafx.scene.layout.HBox
+import scalafx.scene.layout.{FlowPane, HBox, VBox}
 import scalafx.scene.paint.Color._
 import scalafx.scene.paint._
 import scalafx.scene.text.Text
+import java.lang.{Boolean => Jbool}
+
+import scalafx.scene.control.TableColumn.CellEditEvent
+import scalafx.scene.control.cell.CheckBoxTreeTableCell.JBoolean
+
+import scala.util.Random
+
+
+case class Mod(name: String, enabled: Boolean)
+
+case class ObservableMod(name: StringProperty, enabled: BooleanProperty)
+
+object Mod {
+  def asObservableMod(mod: Mod): ObservableMod = {
+    val name = StringProperty(mod.name)
+    val enabled = new BooleanProperty(mod.enabled.asInstanceOf[Jbool], "Enabled", mod.enabled.asInstanceOf[Jbool])
+    ObservableMod(name, enabled)
+  }
+}
+
+object ObservableMod{
+  def asMod(observableMod: ObservableMod): Mod = {
+    Mod(observableMod.name.value, observableMod.enabled.value)
+  }
+}
 
 // How do I want this thing to function???
 // look in mods subdirectory for available mods
@@ -21,35 +51,60 @@ import scalafx.scene.text.Text
 object Modsenfree extends JFXApp {
   stage = new PrimaryStage {
     //    initStyle(StageStyle.Unified)
-    title = "ScalaFX Hello World"
-    scene = new Scene {
-      fill = Color.rgb(38, 38, 38)
-      content = new HBox {
-        padding = Insets(50, 80, 50, 80)
-        children = Seq(
-          new Text {
-            text = "Scala"
-            style = "-fx-font: normal bold 100pt sans-serif"
-            fill = new LinearGradient(
-              endX = 0,
-              stops = Stops(Red, DarkRed))
-          },
-          new Text {
-            text = "FX"
-            style = "-fx-font: italic bold 100pt sans-serif"
-            fill = new LinearGradient(
-              endX = 0,
-              stops = Stops(White, DarkGray)
-            )
-            effect = new DropShadow {
-              color = DarkGray
-              radius = 15
-              spread = 0.25
-            }
-          }
+    title = "Modsenfree Mod Loader"
+    scene = new Scene(600, 600) {
+      //      fill = Color.rgb(38, 38, 38)
+      private val modData: ObservableBuffer[ObservableMod] = ObservableBuffer(
+        Mod.asObservableMod(Mod("bigger text", enabled = true)),
+        Mod.asObservableMod(Mod("run always", enabled = false))
+      )
+      modData.foreach(oMod => {
+        oMod.enabled.onChange{
+          println("Changed" + oMod.name)
+        }
+      })
+
+      private val modTableView: TableView[ObservableMod] = new TableView(modData){
+        editable = true
+        columnResizePolicy = TableView.ConstrainedResizePolicy
+
+
+        private val nameColumn = new TableColumn[ObservableMod, String]("Name"){
+          cellValueFactory = cdf => cdf.value.name
+        }
+        private val enabledColumn: TableColumn[ObservableMod, Jbool] = new TableColumn[ObservableMod, Jbool]("Enabled")
+        {
+          cellValueFactory = _.value.enabled
+            .asInstanceOf[ObservableValue[Jbool, Jbool]]
+          cellFactory = CheckBoxTableCell.forTableColumn(this)
+          editable = true
+          minWidth = 100
+          maxWidth = 100
+        }
+
+        columns ++= Seq(
+          enabledColumn,
+          nameColumn
         )
       }
-    }
+      root = modTableView
 
+    }
   }
 }
+
+
+//new ScrollPane {
+//  // fitToHeight = true
+//  //          padding = Insets(50, 80, 50, 80)
+//  content = new VBox {
+//  //            padding = Insets(50, 80, 50, 80)
+//  children =
+//  (1 to 30).map { i =>
+//  new Text {
+//  text = Random.nextLong().toString
+//  style = "-fx-font: italic bold 100pt sans-serif"
+//}
+//}
+//}
+//}
