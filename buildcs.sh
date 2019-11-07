94508d15a11a66dcbacd4fd06abb41269d679315
Makaque <Makaque@users.noreply.github.com>
2019-11-07 05:03:24 -0700

    test is compiling.

--- buildcs.sh
+++ buildcs.sh
@@ -2,3 +2,4 @@
 
 ./Libraries/Mono/bin/mcs ./src/main/cs/Patcher.cs -r:./Resources/UnityEngine.dll,./Resources/Mono.Cecil.dll,./Resources/Mono.Cecil.Inject.dll
 ./Libraries/Mono/bin/mcs ./src/main/cs/Hook.cs -t:library -r:./Resources/0Harmony.dll
+./Libraries/Mono/bin/mcs ./src/main/cs/TestMod.cs -t:library -r:./Resources/0Harmony.dll,./Resources/Assembly-CSharp.dll,./Resources/UnityEngine.dll,./Resources/UnityEngine.UI.dll
