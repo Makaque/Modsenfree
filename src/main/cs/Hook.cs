f1aaa9934102eb5387c5001aa4932cc6a1daa8bf
Makaque <Makaque@users.noreply.github.com>
2020-01-10 19:35:36 -0700

    Removed logging.

--- src/main/cs/Hook.cs
+++ src/main/cs/Hook.cs
@@ -48,27 +48,18 @@ namespace HookNamespace
             Console.WriteLine(msg);
             string content = "";
             if (File.Exists("./Logsenfree.txt")) {
-
-            content = File.ReadAllText("./Logsenfree.txt");
+                content = File.ReadAllText("./Logsenfree.txt");
             }
             File.WriteAllText("./Logsenfree.txt", content + "\n" + msg);
-            // File.WriteAllText("./Logsenfree.txt", msg);
         }
 
         public static Assembly getModAssembly(FileContents fileContents)
         {
-            log("before data helper");
             DataHelper helper = new DataHelper(fileContents.contents);
-            log("got data helper");
             string assemblyName = helper.GetValue<string>(Field.assembly, "");
-            log("got assemblyName: " + assemblyName);
             if(assemblyName != null){
                 string relativeFile = Path.GetDirectoryName(fileContents.file) + Path.DirectorySeparatorChar + assemblyName;
-            log("got dirname");
                 string absoluteFile = Path.GetFullPath(relativeFile);
-            log("got full path");
-                log("absolue file");
-                log(absoluteFile);
                 return Assembly.LoadFile(absoluteFile);
             }
             return null;
@@ -127,16 +118,12 @@ namespace HookNamespace
         }
 
         public static List<Pair<FileContents,Assembly>> getAssemblies(List<FileContents> fileContents){
-            log("in getassemblies");
             List<Pair<FileContents,Assembly>> assemblies = new List<Pair<FileContents,Assembly>>();
             foreach (var fc in fileContents)
             {
-            log("for fc");
                 Assembly asm = Hook.getModAssembly(fc);
-            log("got mod assembly");
             // Super important. Code implodes if you try to compare asm to null directly.
                 if(!((object) asm).Equals(null)){
-                    log("asm not null");
                     assemblies.Add(new Pair<FileContents, Assembly>(fc, asm));
                 }
             }
@@ -144,17 +131,12 @@ namespace HookNamespace
         }
 
         public static List<Pair<FileContents,Assembly>> getMods()
-        // public static void getMods()
         {
-            // string[] dirs = Directory.GetDirectories(modsDirectory);
-            // return 
-            log("in getmods");
             string[] dirs = Directory.GetDirectories(modsDirectory);
             List<string> files = Hook.getDirFiles(dirs);
             List<string> settingsFiles = Hook.filterSettingsFiles(files);
             List<FileContents> fileContents = Hook.getFileContents(settingsFiles);
             List<FileContents> enabled = Hook.filterEnabled(fileContents);
-            log("about to get assemblies");
             List<Pair<FileContents,Assembly>> assemblies = Hook.getAssemblies(enabled);
             return assemblies;
             // Func<string,string> bla = (a) => "hi";
@@ -172,19 +154,12 @@ namespace HookNamespace
             try{
             // Current directory is Oxenfree top-level, not Assembly-CSharp.dll location
             // HarmonyInstance harmony = HarmonyInstance.Create("ca.mktsk.modsenfree.loader");
-            log("call getmods");
-            // Hook.getMods();
-            // Assembly asm = Assembly.LoadFrom(@"C:\Program Files (x86)\Steam\steamapps\common\Oxenfree\Mods\testmod\TestMod.dll");
-            // harmony.PatchAll(Assembly.GetExecutingAssembly());
             foreach (var mod in Hook.getMods())
             {
-                log("mod");
                 try{
                     FileContents fileContents = mod.left;
                     Assembly asm = mod.right;
-                    log("Contents = "+ fileContents.contents);
                     String modId = getModIdentifier(fileContents.contents);
-                    log("ModId = "+ modId);
                     if (!modId.Equals("")){
                         HarmonyInstance harmonyInstance = HarmonyInstance.Create(modId);
                         harmonyInstance.PatchAll(asm);
@@ -194,21 +169,10 @@ namespace HookNamespace
                     log(e.StackTrace);
                 }
             }
-            log("Success");
 
             } catch (Exception e){
                 log(e.StackTrace);
             }
-            // try
-            // {
-            //     // Console.WriteLine(Hook.getMods().ToString());
-            //     throw new Exception("blowing up");
-            // }
-            // catch (Exception e)
-            // {
-            //     System.IO.File.WriteAllText(@"./Logsenfree.txt", e.StackTrace);
-
-            // }
         }
     }
 }
